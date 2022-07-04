c8cd0176db1299abb20c53dc2aa2c74a0e6ec4bf
r3X <rch246chauhan@gmail.com>
2022-07-04 20:35:38 +0530

    Commit

---
+++ src/UI/Header.svelte
@@ -0,0 +1,25 @@
+<header>
+  <h1>MeetUp</h1>
+</header>
+
+<style>
+  header {
+    display: flex;
+    position: fixed;
+    align-items: center;
+    height: 5rem;
+    background-color: #2947f2;
+    top: 0;
+    left: 0;
+    width: 100vmax;
+    justify-content: center;
+  }
+
+  h1 {
+    margin-left: 2rem;
+    font-family: "Pacifico", cursive;
+    font-weight: 100;
+
+    color: #ed901e;
+  }
+</style>
